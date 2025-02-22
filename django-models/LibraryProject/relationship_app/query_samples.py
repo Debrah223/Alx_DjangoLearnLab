71cc1cde1971e7ff50828aa0017a26951d62a1e5
Debrah223 <debrahmachuki21@gmail.com>
2025-02-22 12:51:21 +0300

    Added get function on retrieve and librarian functions

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app import Author, Book, Library, Librarian
 
 # Query to get all books by a specific author
 def get_books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
+    author = Author.objects.get(name=author_name).first()
     if author:
         return Book.objects.filter(author=author)
     return None
@@ -16,7 +16,7 @@ def get_books_in_library(library_name):
 
 # Query to retrieve the librarian for a Library
 def get_librarian_for_library(library_name):
-    library = Library.objects.filter(name=library_name).first()
+    library = Library.objects.get(name=library_name).first()
     if library:
         return Librarian.objects.filter(library=library).first()
     return None
