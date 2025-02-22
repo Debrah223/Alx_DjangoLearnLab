45130259deb0c96fb1ec17ad1b5e7f20707888cf
Debrah223 <debrahmachuki21@gmail.com>
2025-02-22 13:51:48 +0300

    Added render request on views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,13 +6,12 @@ from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
-    book_details = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(f"Books List:\n{book_details}")
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 from django.views.generic import DetailView
 from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "libraru_detail.html" # this will be the template we shall use to render the output
+    template_name = "library_detail.html" # this will be the template we shall use to render the output
     context_object_name = "library"
