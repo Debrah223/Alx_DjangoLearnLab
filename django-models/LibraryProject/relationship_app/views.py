a9cba2b8724eb4438fc25311bb4fbd2b9bc13d3e
Debrah223 <debrahmachuki21@gmail.com>
2025-02-22 13:42:53 +0300

    Added django views and url configurations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from .models import Book
+
+def book_list(request):
+    books = Book.objects.all()
+    book_details = "\n".join([f"{book.title} by {book.author.name}" for book in books])
+    return HttpResponse(f"Books List:\n{book_details}")
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "libraru_detail.html" # this will be the template we shall use to render the output
+    context_object_name = "library"
