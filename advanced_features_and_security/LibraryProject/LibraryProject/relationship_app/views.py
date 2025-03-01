53c5ee1cfc892eec8c4ef22d5b32509d1cb0a29e
Debrah223 <debrahmachuki21@gmail.com>
2025-03-01 21:19:30 +0300

    Added book list in views

--- advanced_features_and_security/LibraryProject/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/relationship_app/views.py
@@ -124,3 +124,8 @@ def delete_book(request, book_id):
         book.delete()
         return redirect("book_list")
     return render(request, "relationship_app/delete_book.html", {"book": book})
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
