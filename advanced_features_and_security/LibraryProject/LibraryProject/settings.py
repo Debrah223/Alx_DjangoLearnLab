6d0470c0e8a9c220ac8e628f457d59ab25ce85cb
Debrah223 <debrahmachuki21@gmail.com>
2025-03-01 22:06:45 +0300

    Added templates

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "LibraryProject/bookshelf/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
