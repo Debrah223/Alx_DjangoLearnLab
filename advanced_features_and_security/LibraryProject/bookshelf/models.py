b2f2ac1159a14f8dedeb49c8c4be18c9efa16ec7
Debrah223 <debrahmachuki21@gmail.com>
2025-03-01 13:51:09 +0300

    Added class in models file

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,25 @@
-from django.db import models
+#from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+#class Book(models.Model):
+   # title = models.CharField(max_length=200)
+    #author = models.CharField(max_length=100)
+    #publication_year = models.IntegerField()
 
-    def __str__(self):
-        return self.title
+    #def __str__(self):
+        #return self.title
+    
+from django.db import models   
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+     email = models.EmailField(unique=True)
+     date_of_birth = models.DateField(null=True, blank=True)
+     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+
+     USERNAME_FIELD = 'email'
+     REQUIRED_FIELDS = ['profile_photo', 'date_of_birth']
+    
+     def __str__(self):
+        return self.email
