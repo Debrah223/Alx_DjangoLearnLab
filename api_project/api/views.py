8370a0780fdaedf8ca4a4ddf601ddd62d139fc03
Debrah223 <debrahmachuki21@gmail.com>
2025-03-06 12:49:30 +0300

    Added generics to booklist class

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,8 @@
 #from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework import generics, ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 # Create your views here.
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all() #to retrieve all books from the db
     serializer_class = BookSerializer # use bookserializer to format data
