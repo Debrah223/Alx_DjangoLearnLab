d0c4a8c0d451cabe4f9795f8e6773a00c4de070e
Debrah223 <debrahmachuki21@gmail.com>
2025-03-23 22:24:09 +0300

    Added correct import for tag

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,8 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post, Tag
-from .models import Comment
+from .models import Post, Comment
 from taggit.forms import TagWidget
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
