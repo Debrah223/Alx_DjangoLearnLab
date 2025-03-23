45139b2b8c4c41c2cd93f919813e3ebbb04144e1
Debrah223 <debrahmachuki21@gmail.com>
2025-03-23 21:06:45 +0300

    Added taggit to post form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Tag
 from .models import Comment
-
+from taggit.forms import TagWidget
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -19,6 +19,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]  # Exclude 'author' because it's set automatically
+        widgets = {
+            "tags": TagWidget(attrs={"class": "form-control", "placeholder": "Add tags separated by commas"}),
+        }
 
     def save(self, commit=True):
         post = super().save(commit=False)
