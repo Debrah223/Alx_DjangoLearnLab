82e70124fc762c2f2c9c1729c5a1bb1cadf59c22
Debrah223 <debrahmachuki21@gmail.com>
2025-03-23 21:17:25 +0300

    Modified forms file

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -17,7 +17,6 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     tags = TaggableManager()
 
-
     def __str__(self):
         return self.title
 
@@ -30,7 +29,6 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post}"
-
 class Post(models.Model):
     # Existing fields...
 
